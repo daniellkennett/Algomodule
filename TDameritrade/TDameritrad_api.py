ef1af7f78c2e9fb1a78dd27890e2ff814c709457
Daniel L. Kennett <69829253+daniellkennett@users.noreply.github.com>
2021-12-30 16:43:09 -0500

    Delete TDameritrad_api.py

--- TDameritrade/TDameritrad_api.py
+++
@@ -1,4 +0,0 @@
-CONSUMER_KEY = 'LAGNY3YGWVSGN5LUJASFNNRRC10FUF70@AMER.OAUTHAP'
-REDIRECT_URI = 'http://localhost'
-JSON_PATH = '/home/daniel/Desktop/Python/Projects/Algomodule/TDameritrade/key.json'
-
