43be2c0d78e108151c659c87fb11d88ebc6b0b95
Danielkennett <daniel.l.kennett@gmail.com>
2022-01-09 20:42:00 -0500

    add Fractional Differencing

--- src/TechAnalysis.py
+++ src/TechAnalysis.py
@@ -1,3 +1,7 @@
+from statsmodels.tsa.stattools import adfuller
+import numpy as np
+import pandas as pd
+
 class TechAnalysis:
     """
     A class to run Technical Analysis on a Pandas Dataframe
@@ -57,3 +61,88 @@ class TechAnalysis:
     def fib_retracement(self, column_name, days, fib=1):
         #23.6%, 38.2%, 50%, 61.8%, and 100%#
         return None
+    
+    
+    """
+    Use Fractional Differencing to create stationary data for predictions
+    Data offsets by weights_window_size
+    """
+    
+    @staticmethod
+    def get_weights_floored(d, num_k, floor=1e-3):
+        r"""Calculate weights ($w$) for each lag ($k$) through
+        $w_k = -w_{k-1} \frac{d - k + 1}{k}$ provided weight above a minimum value
+        (floor) for the weights to prevent computation of weights for the entire
+        time series.
+
+        Args:
+            d (int): differencing value.
+            num_k (int): number of lags (typically length of timeseries) to calculate w.
+            floor (float): minimum value for the weights for computational efficiency.
+        """
+        w_k = np.array([1])
+        k = 1
+
+        while k < num_k:
+            w_k_latest = -w_k[-1] * ((d - k + 1)) / k
+            if abs(w_k_latest) <= floor:
+                break
+
+            w_k = np.append(w_k, w_k_latest)
+
+            k += 1
+
+        w_k = w_k.reshape(-1, 1) 
+
+        return w_k
+    
+    @staticmethod
+    def frac_diff(df, d, floor=1e-3):
+        r"""Fractionally difference time series via CPU.
+
+        Args:
+            df (pd.DataFrame): dataframe of raw time series values.
+            d (float): differencing value from 0 to 1 where > 1 has no FD.
+            floor (float): minimum value of weights, ignoring anything smaller.
+        """
+        # Get weights window
+        weights = TechAnalysis.get_weights_floored(d=d, num_k=len(df), floor=floor)
+        weights_window_size = len(weights)
+
+        # Reverse weights
+        weights = weights[::-1]
+
+        # Blank fractionally differenced series to be filled
+        df_fd = []
+
+        # Slide window of time series, to calculated fractionally differenced values
+        # per window
+        for idx in range(weights_window_size, df.shape[0]):
+            # Dot product of weights and original values
+            # to get fractionally differenced values
+            date_idx = df.index[idx]
+            df_fd.append(np.dot(weights.T, df.iloc[idx - weights_window_size:idx]).item())
+
+        # Return FD values and weights
+        df_fd = pd.DataFrame(df_fd)
+
+        return df_fd, weights
+
+    def fractional_difference(self, column_name, alpha):
+        """
+        Run iterations of fractional differing to find best d_value.
+        Do this by running adfuller test. A test of Stationarity. Low p_value = Stationarity
+        Returns values, weights, and d_value of best_d_value
+        """
+        adfs = []
+
+        for d in np.linspace(0,1,51):
+            df_fd, weights = TechAnalysis.frac_diff(self.df[column_name], d)
+            pvalue = adfuller(df_fd.values)[1]
+            adfs.append([d, pvalue])
+            if pvalue < alpha:
+                break      
+        best_d_value = adfs[-1][0]
+        
+        
+        return df_fd, weights, best_d_value
