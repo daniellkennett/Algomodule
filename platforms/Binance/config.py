9ff56125ccf0f458357c386777dbc2981fbc075f
Daniel L. Kennett <69829253+daniellkennett@users.noreply.github.com>
2022-04-09 14:33:36 -0400

    Delete config.py

--- platforms/Binance/config.py
+++
@@ -1,6 +0,0 @@
-"""
-Binance US API key and secret key
-"""
-
-key = '892RL9VyYzZsYhXcRbUiuOXz4kswctCFKXU51LdOOuQXHOrPSG6Pfy96gcIrzTcc'
-secret_key = '8FKwPtzdHn6pujV4kx2NKjAzAwSNd0oOE9HU3m79qghEkvRC90YwvNYVbdvuTw1l'
